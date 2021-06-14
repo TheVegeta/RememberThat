816aa2687e519f9a99d434db5bc06bc713aa5ebc
TheVegeta <iamvegeta@protonmail.com>
2021-06-14 11:33:49 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const mobilebtn = document.getElementById("mobile-cta");
+const nav = document.querySelector("nav");
+const mobileBtnExit = document.getElementById("mobile-exit");
+
+mobilebtn.addEventListener("click", () => {
+  nav.classList.add("menu-btn");
+});
+
+mobileBtnExit.addEventListener("click", () => {
+  nav.classList.remove("menu-btn");
+});
